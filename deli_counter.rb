4b45236aa3d0f3f4971384070539efe8e25f41f1
Jen <jenniferanndcb@gmail.com>
2019-05-02 08:36:03 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,7 @@
-# Write your code here.
+def line 
+  puts "The line is currently empty."
+end 
+
+def take_a_number (line, name)
+  line.collect do |name| 
+    puts "Welcome, #{name}. You are number "
