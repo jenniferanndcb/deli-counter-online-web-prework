bf1eb212eca69cfbca0a295cb9379cff382a9c4f
Jen <jenniferanndcb@gmail.com>
2019-05-02 08:46:36 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,10 +1,13 @@
 def line (katz_deli)
   if katz_deli = []
     puts "The line is currently empty."
+  else 
+    katz_deli.each do {|name, i|
+    puts "The line is currently: #{i}. #{name}"
   end
 end 
 
-def take_a_number (line, name)
+def take_a_number (katz_deli, name)
   line.collect do |name| 
     puts "Welcome, #{name}. You are number #{line} in line."
   end
